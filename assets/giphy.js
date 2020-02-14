20a268206e09b3497bc8abb70cdd069473726479
Jacob Erickson <jakedasnake6_12@hotmail.com>
2020-02-14 16:34:00 -0700

    Updated the html, css, and JavaScript

--- assets/giphy.js
+++ assets/giphy.js
@@ -1,32 +1,92 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-var topics = ['rhinoceros', 'hippopotamus', 'lion', 'tiger', 'bear', 'elephant', 'cheetah'];
+    var topics = ['rhinoceros', 'hippopotamus', 'lion', 'tiger', 'bear', 'elephant', 'cheetah'];
 
-var api = "";
-var apiKey = "zcBHe2DN8JC1y667Dg0ol2uNl1ntMX9A";
-var query = "";
 
-var searchBar = document.getElementById("searchBar");
+    var apiKey = "zcBHe2DN8JC1y667Dg0ol2uNl1ntMX9A";
+ 
 
-$('.info').empty();
-$("#gifs-appear-here").empty();
+    var searchBar = document.getElementById("searchBar");
 
-function createbuttons () {
-$('.buttons').empty();
-}
+    $('.info').empty();
+    $("#gifs-appear-here").empty();
 
-function displaying () {}
-//apikey = zcBHe2DN8JC1y667Dg0ol2uNl1ntMX9A
-$.ajax({
-url: queryURL,
-Method: "GET"
-}).then(function (response) {
-    
-    var rating = response.data[i].rating
-    var title = response.data[i].title
-}
 
-createbuttons();
-$(document).on('click', '.images', stillAnimate)
-$(document).on('click', '.btn', displaying)
-});
+    // rendering the buttons and handleing the on click
+    function createbuttons() {
+        $('#topics').empty();
+
+        for (var i = 0; i < topics.length; i++) {
+            var btn = `<button class="searchtopic" index=${i}>${topics[i]}</button>`
+            $("#topics").append(btn)
+        }
+        $(".searchtopic").on("click", function () {
+            console.log(this)
+            // get the index info from the btn to find the topic on the aarray to search for this topic
+            var index = $(this).attr("index")
+            var topic = topics[index]
+            console.log(topic, index)
+
+            displaying(topic)
+
+        })
+
+    }
+
+    // this is in charge of call api giphy with the topic and render the images
+    function displaying(topic) {
+        //apikey = zcBHe2DN8JC1y667Dg0ol2uNl1ntMX9A
+        var queryURL = `https://api.giphy.com/v1/gifs/search?q=${topic}&api_key=${apiKey}&limit=10`;
+        $.ajax({
+            url: queryURL,
+            Method: "GET"
+        }).then(function (response) {
+            console.log(response.data)
+
+            //with the object received from giphy we render the images
+            // keeping two urls from the object one animated one still 
+            $("#gifs").empty()
+            for (var i = 0; i < response.data.length; i++) {
+
+                var card = `<div class="col"><div class="card" style="width: height:16rem>
+                <img class="imggifs card-img-top" src=${response.data[i].images.fixed_height_still.url} alt-src=${response.data[i].images.downsized_large.url}></img> 
+                <div class="card-body">
+                  <p class="card-text">${response.data[i].rating}</p>
+                </div>
+              </div></div>`
+              //  var rating = `<p>${response.data[i].rating}`
+              //  var img = `<img class="imggifs" src=${response.data[i].images.fixed_height_still.url} alt-src=${response.data[i].images.downsized_large.url}></img> `
+                $("#gifs").append(card)
+            }
+        
+            // handleing the onclick on the images
+            // everytime image is clicked we switch the src then the effect is like we are stoping and starting the images but is just an effect because we are swith the src the brwoser is seeing
+
+            $(".imggifs").on("click", function(){
+                console.log(this)
+                var src = $(this).attr("src")
+                var alt = $(this).attr("alt-src")
+                $(this).attr("src", alt)
+                $(this).attr("alt-src", src)
+            })
+            //   var rating = response.data[i].rating
+            //   var title = response.data[i].title
+        })
+    }
+
+    createbuttons()
+
+// this is in charge of the form, when search btn is clicked we read the info from the input and we
+    $("#search").on("click", function () {
+        event.preventDefault()
+        var topic = $("#searchtopic").val()
+        $("#searchtopic").empty()
+        console.log("search", topic)
+        displaying(topic)
+        topics.push(topic)
+        createbuttons()
+
+    })
+
+
+})
